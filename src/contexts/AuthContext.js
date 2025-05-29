7bbaa9ebc08e6c168e53a85052a3d895b629b23f
JoseDeyvid <j_deyvid@live.com>
2025-05-29 17:18:34 -0300

    Adicionar loading

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,15 +1,33 @@
 import { createContext, useState } from "react";
+import api from "../services/api";
+import { useNavigation } from "@react-navigation/native";
 
 
 const AuthContext = createContext({});
 
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState({
-        name: "Nome de teste"
-    })
+    const navigation = useNavigation();
+    const [loading, setLoading] = useState(false);
+
+    const signup = async (name, email, password) => {
+        try {
+            setLoading(true)
+            const res = await api.post('/users', {
+                name,
+                email,
+                password
+            })
+            console.log("Data: ", res.data);
+            setLoading(false)
+            navigation.goBack();
+        } catch (error) {
+            console.log("Erro: ", error)
+            setLoading(false)
+        }
+    }
     return (
-        <AuthContext.Provider value={{user}}>
+        <AuthContext.Provider value={{ signup, loading }}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,27 +1,40 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { AreaInput, Background, Container, Input, SubmitButton, SubmitText } from "../SignIn/styles"
 import AuthContext from "../../contexts/AuthContext"
+import { ActivityIndicator } from "react-native";
 
 
 const SignUp = () => {
-  const { user } = useContext(AuthContext);
+  const { signup, loading } = useContext(AuthContext);
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const handleSignUp = () => {
-    console.log(user.name)
+    if (name && email && password) {
+      signup(name, email, password)
+      return
+    }
   }
+
   return (
     <Background>
       <Container>
         <AreaInput>
-          <Input placeholder="Seu nome" />
+          <Input placeholder="Seu nome" value={name} onChangeText={(text) => setName(text)} />
         </AreaInput>
         <AreaInput>
-          <Input placeholder="Email" />
+          <Input placeholder="Email" value={email} onChangeText={(text) => setEmail(text)} />
         </AreaInput>
         <AreaInput>
-          <Input placeholder="Senha" />
+          <Input placeholder="Senha" secureTextEntry value={password} onChangeText={(text) => setPassword(text)} />
         </AreaInput>
-        <SubmitButton onPress={handleSignUp}>
-          <SubmitText>Cadastrar</SubmitText>
+        <SubmitButton onPress={handleSignUp} disabled={loading}>
+          {loading ?
+            <ActivityIndicator size={20} color={'#fff'} />
+            :
+            <SubmitText>Cadastrar</SubmitText>
+          }
         </SubmitButton>
       </Container>
     </Background>
